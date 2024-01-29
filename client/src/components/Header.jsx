db2a89f6bc6be4bedc9918305070fa267c650a5a
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-29 15:55:25 +0500

    sign up

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
                 className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
             >
                 <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                    Aliyev's
+                    Nelo's
                 </span>
                 Blog
             </Link>
@@ -25,7 +25,7 @@ export default function Header() {
                     className='hidden lg:inline'
                 />
             </form>
-            <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+            <Button className='w-12 h-10 lg:hidden' color='gray'>
                 <AiOutlineSearch />
             </Button>
             <div className='flex gap-2 md:order-2'>
@@ -35,7 +35,7 @@ export default function Header() {
                 >
                     <FaMoon />
                 </Button>
-                <Link to='/signin'>
+                <Link to='/signup'>
                     <Button gradientDuoTone='purpleToBlue' outline>
                         Sign In
                     </Button>
