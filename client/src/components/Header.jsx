7316f839aa18994ec1dbb5c0e2bf403dca10fdea
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-01-30 11:49:42 +0500

    signout functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,12 +3,34 @@ import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 export default function Header() {
     const path = useLocation().pathname;
     const { currentUser } = useSelector((state) => state.user)
-    console.log(currentUser);
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
 
+    const handleSignout = async () => {
+        try {
+            const res = await fetch('/api/user/signout', {
+                method: 'POST',
+            });
+            const data = await res.json();
+            if (!res.ok) {
+                console.log(data.message);
+            } else {
+                dispatch(signoutSuccess());
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
     return (
         <Navbar className='border-b-2'>
             <Link
@@ -57,7 +79,7 @@ export default function Header() {
                                 <Dropdown.Item>Profile</Dropdown.Item>
                             </Link>
                             <Dropdown.Divider />
-                            <Dropdown.Item>Sign out</Dropdown.Item>
+                            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
                         </Dropdown>
                     ) : (
                         <Link to='/signup'>
