00f6d810f7507212f2ad0e2515af12f15fb70659
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-02-02 10:28:41 +0500

    Users page for only admin

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
 export default function Dashboard() {
     const location = useLocation();
     const [tab, setTab] = useState('');
@@ -23,6 +24,8 @@ export default function Dashboard() {
             {tab === 'profile' && <DashProfile />}
             {/* posts... */}
             {tab === 'posts' && <DashPosts />}
+            {/* users */}
+            {tab === 'users' && <DashUsers />}
         </div>
     );
 }
