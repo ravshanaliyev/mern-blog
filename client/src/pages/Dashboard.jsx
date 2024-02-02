209396d5d355e7606a5a7d8fd7276790861598ec
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-02-02 11:21:35 +0500

    Dashboard statistic section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardCamp';
 export default function Dashboard() {
     const location = useLocation();
     const [tab, setTab] = useState('');
@@ -29,6 +30,8 @@ export default function Dashboard() {
             {tab === 'users' && <DashUsers />}
             {/* comments */}
             {tab === 'comments' && <DashComments />}
+            {/* overview */}
+            {tab === 'dash' && <DashboardComp />}
         </div>
     );
 }
