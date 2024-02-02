a369f9db99833ba526862a1a4c365d78faebc786
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-02-02 10:32:25 +0500

    post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./components/PrivateRoute"
 import CreatePost from "./pages/CreatePost"
 import AdminPrivateRoute from "./components/AdminPrivateRoute"
 import UpdatePost from "./pages/UpdatePost"
+import PostPage from "./pages/PostPage"
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
